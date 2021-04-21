d0649c1ed24efe8d8449b845b1f2e44a98a57ea9
ivana <ivananl@yahoo.com>
2021-04-21 12:07:36 +0200

    Add bubble_sort fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,12 +5,12 @@ def bubble_sort(arr)
   change = true
 
   while change
+    change = false
     i = 0
     while i < l - 1
       if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      else
-        change = false
+        change = true
       end
       i += 1
     end
@@ -23,15 +23,20 @@ def bubble_sort_by(arr)
   change = true
 
   while change
+    change = false
     i = 0
     while i < l - 1
       if (yield arr[i], arr[i + 1]).positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      else
-        change = false
+        change = true
       end
       i += 1
     end
   end
   arr
 end
+
+a = [8, 7, 6, 5, 4, 4, 2, 1, 9, 10]
+p bubble_sort(a)
+
+p (bubble_sort_by(unsorted) { |left, right| left.length - right.length }).to eq %w[h hi hey hiya hello]
