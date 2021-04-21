d09d091746a82604d5eb04a7ede83d504f63d031
TedLivist <livistteddy@gmail.com>
2021-04-21 09:40:48 +0100

    Syntax fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,21 @@
 def bubble_sort(arr)
- l=arr.length
- if l<=1
-  return arr
- end
+  l = arr.length
+  return arr if l <= 1
 
- change = true
+  change = true
 
- while change
-  i = 0
-  while i < l - 1
-    if arr[i] > arr[i + 1]
-      arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    else
-      change = false
+  while change
+    i = 0
+    while i < l - 1
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      else
+        change = false
+      end
+      i += 1
     end
-    i += 1
   end
- end
- arr
+  arr
 end
 
 a = [2, 5, 18, 7, 3, 14]
